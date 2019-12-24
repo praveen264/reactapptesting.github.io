0fe9f1a153fb1242cc0998af7779093db84cf7a5
praveen264 <pstraveen264@gmail.com>
2019-12-24 15:23:36 +0530

    publish
    wow

---
+++ precache-manifest.0595e148b2418a1a1fdee0ae755f7876.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11655de0ccec1426c7f76556f5738a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "157e127db734d5999ce4",
+    "url": "/static/css/2.5c4d1152.chunk.css"
+  },
+  {
+    "revision": "d27d891f9621acc506b7",
+    "url": "/static/css/main.edcb6c02.chunk.css"
+  },
+  {
+    "revision": "157e127db734d5999ce4",
+    "url": "/static/js/2.d00272ad.chunk.js"
+  },
+  {
+    "revision": "d27d891f9621acc506b7",
+    "url": "/static/js/main.61119589.chunk.js"
+  },
+  {
+    "revision": "ede79f7df5547bdf4db8",
+    "url": "/static/js/runtime-main.e1bc48e9.js"
+  }
+]);
